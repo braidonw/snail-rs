43170fa6043bbf81932148c3e2a5e4243b781b5d
braidonw <braidon@braidonwhatley.com>
2023-02-16 11:32:30 +1100

    chapter 3

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use snail::startup::run;
+use std::net::TcpListener;
+use snail::configuration::get_configuration;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
+}
+
