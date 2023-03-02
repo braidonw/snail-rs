de1efa4eb3aaea01b9f4fd00910d3dc03b1fc65d
braidonw <braidon@braidonwhatley.com>
2023-03-03 10:15:16 +1100

    fixed tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -96,12 +96,8 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app = spawn_app().await;
-    let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_string = configuration.database.connection_string();
-    let mut connection = PgConnection::connect(&connection_string)
-        .await
-        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
+    let mut connection = app.db_pool.acquire().await.unwrap();
 
     // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
