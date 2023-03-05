875f2097627648d1a3b993c66b8deb294935ee35
braidonw <braidon@braidonwhatley.com>
2023-03-05 21:20:30 +1100

    wip chapter 7

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -60,14 +60,20 @@ async fn spawn_app() -> TestApp {
     configuration.database.database_name = Uuid::new_v4().to_string();
     let connection_pool = configure_database(&configuration.database).await;
 
-    let sender_email = configuration.email_client.sender().expect("Invalid sender email address");
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address");
+    let timeout = configuration.email_client.timeout();
     let email_client = EmailClient::new(
         configuration.email_client.base_url,
         sender_email,
         configuration.email_client.authorization_token,
+        timeout,
     );
 
-    let server = run(listener, connection_pool.clone(), email_client).expect("Failed to bind to address");
+    let server =
+        run(listener, connection_pool.clone(), email_client).expect("Failed to bind to address");
     let _ = tokio::spawn(server);
 
     TestApp {
