e168e207b05e6ffc0a607666994fbd7bda5805f1
braidonw <braidon@braidonwhatley.com>
2023-03-06 10:16:37 +1100

    p284 wip

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,11 +1,10 @@
 use once_cell::sync::Lazy;
 use snail::configuration::{get_configuration, DatabaseSettings};
-use snail::email_client::EmailClient;
 use snail::startup::{get_connection_pool, Application};
 use snail::telemetry::{get_subscriber, init_subscriber};
 use sqlx::{Connection, Executor, PgConnection, PgPool};
-use std::net::TcpListener;
 use uuid::Uuid;
+use wiremock::MockServer;
 
 static TRACING: Lazy<()> = Lazy::new(|| {
     let default_filter_level = "info".to_string();
@@ -23,6 +22,7 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
+    pub email_server: MockServer,
 }
 
 impl TestApp {
@@ -42,10 +42,13 @@ pub async fn spawn_app() -> TestApp {
     // Tracing
     Lazy::force(&TRACING);
 
+    let email_server = MockServer::start().await;
+
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration.");
         c.database.database_name = Uuid::new_v4().to_string();
         c.application.port = 0;
+        c.email_client.base_url = email_server.uri();
         c
     };
 
@@ -60,6 +63,7 @@ pub async fn spawn_app() -> TestApp {
     TestApp {
         address,
         db_pool: get_connection_pool(&configuration.database),
+        email_server,
     }
 }
 
